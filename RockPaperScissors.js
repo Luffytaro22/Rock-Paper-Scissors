177d3f8d6bae8119020900b3eb204f747b641cb3
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-15 10:09:40 -0500

    Added animation when the user clicks the images and is not redirected no a new page anymore when he loss or wins.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -25,40 +25,65 @@ let scissors = document.getElementById('scissors');
 let player = document.getElementById('player');
 let computer = document.getElementById('computer');
 let result = document.getElementById('result');
-
+let winner = document.querySelector('.winner');
+let losser = document.querySelector('.losser');
+let main = document.querySelector('.main');
+let buttons = document.querySelectorAll('button');
 
 paper.addEventListener('click', function() {
-    playRound('paper', computerPlay())
+    playRound('paper', computerPlay());
+    paper.classList.add('playing');
+    paper.addEventListener('transitionend', () => {
+        paper.classList.remove('playing');
+    });
     player.innerHTML = countP;   //Para mostrar las variables en el cuerpo del documento dentro de la etiqueta span.
     computer.innerHTML = countM;
     if (countP === 5) {
-        location.href='Game/Winner.html';
+        winner.classList.remove('winner');
+        main.style.display = 'none';
     } else if (countM === 5) {
-        location.href='Game/Losser.html';
+        losser.classList.remove('losser');
+        main.style.display = 'none';
     }
 });
 
 rock.addEventListener('click', function() {
-    playRound('rock', computerPlay())
+    playRound('rock', computerPlay());
+    rock.classList.add('playing');
+    rock.addEventListener('transitionend', () => {    //Se añade un event listener cuando termina la animación y se remove la clase.
+        rock.classList.remove('playing');
+    });
     player.innerHTML = countP;
     computer.innerHTML = countM;
     if (countP === 5) {
-        location.href='Game/Winner.html';
+        winner.classList.remove('winner');   //Si gana, se muestra el div "winner" y se esconde el div principal.
+        main.style.display = 'none';
     } else if (countM === 5) {
-        location.href='Game/Losser.html';
+        losser.classList.remove('losser');    //Si pierde, se muestra el div "losser" y se esconden los demás.
+        main.style.display = 'none';
     }
 });
 
 scissors.addEventListener('click', function() {
-    playRound('scissors', computerPlay())
+    playRound('scissors', computerPlay());
+    scissors.classList.add('playing');
+    scissors.addEventListener('transitionend', () => {
+        scissors.classList.remove('playing');
+    });
     player.innerHTML = countP;
     computer.innerHTML = countM;
     if (countP === 5) {
-        location.href='Game/Winner.html';
+        winner.classList.remove('winner');
+        main.style.display = 'none';
     } else if (countM === 5) {
-        location.href='Game/Losser.html';
+        losser.classList.remove('losser');
+        main.style.display = 'none';
     }
 });
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    location.reload();
+}));
 
 
 
